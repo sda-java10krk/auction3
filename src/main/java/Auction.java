69169302c130b9e4869acdb56d425c84381057e0
Bartosz Kupajski <bartosz.kupajski@gmail.com>
2018-07-20 20:20:39 +0200

    Dodane Składanie oferty

--- src/main/java/Auction.java
+++ src/main/java/Auction.java
@@ -1,10 +1,15 @@
+import Exceptions.OfferTooLowException;
 import Exceptions.SubcategoryPresentException;
 
+import java.util.List;
+
 public class Auction {
-    String title;
-    String description;
-    Long startingPrice;
-    Category category;
+    private String title;
+    private String description;
+    private Long startingPrice;
+    private Category category;
+    private List<Offers> offersList;
+    private Offers currentOffert;
 
     public Auction(String title, String description, Long startingPrice, Category category) throws SubcategoryPresentException {
         this.title = title;
@@ -17,15 +22,33 @@ public class Auction {
         }
     }
 
-    public void currentOffer(){
-        
+    public void addingOffer(Offers offer) throws OfferTooLowException{
+        if(this.currentOffert==null && currentOffert.getPrice()<offer.getPrice()){
+            this.currentOffert = offer;
+            this.offersList.add(offer);
+        }
+        throw new OfferTooLowException();
     }
 
+    public String getTitle() {
+        return title;
+    }
 
+    public String getDescription() {
+        return description;
+    }
 
+    public Long getStartingPrice() {
+        return startingPrice;
+    }
 
+    public Category getCategory() {
+        return category;
+    }
 }
 /*
 Każdy użytkownik może wystawić aukcję. Aukcja ma: tytuł, opis, cenę początkową, kategorię.
 Każde z tych pól może mieć wartość, która się powtarza, tj. np. mogą być w systemie dwie aukcje "Opel Astra" za 1000zł tego samego użytkownika.
+
+
  */
---
+++ src/main/java/Exceptions/OfferTooLowException.java
@@ -0,0 +1,4 @@
+package Exceptions;
+
+public class OfferTooLowException extends Exception {
+}
