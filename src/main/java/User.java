67725dddc9d34badb1e144732079beec2377b0cf
Filip <Filipras@vp.pl>
2018-07-20 19:16:45 +0200

    Tworzenie konta

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,6 +1,31 @@
+import java.util.*;
+
 public class User {
     String login;
     String password;
+
+
+    List<User> userList = new ArrayList<User>();
+
+
+    public void createUser(String login, String password) throws IllegalArgumentException  {
+
+        System.out.println("Podaj swoj login: ");
+        Scanner scan = new Scanner(System.in);
+        login= scan.next();
+        for(int i = 0; i<= userList.size();i++){
+            if(userList.get(i).login.equals(login)){
+                throw new IllegalArgumentException();
+            }
+        }
+            System.out.println("Podaj haslo:");
+            password = scan.next();
+            if (password.length() < 5) {
+                throw new IllegalArgumentException();
+            }
+    }
+
+
 
     public void makeAnOffer(long offer){
 
