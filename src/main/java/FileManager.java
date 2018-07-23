c7b8ccf904d96fa8962a849f7b77908238eff922
Marcin <zapieful@gmail.com>
2018-07-23 19:27:42 +0200

    Update3

--- src/main/java/FileManager.java
+++ src/main/java/FileManager.java
@@ -1,18 +1,16 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class FileManager {
-
-
 
     public void saveUserToList (User user) {
 
         String fileName1 = "UserList.txt";
             try {
-                FileReader fileReader = new FileReader(fileName1);
-                BufferedReader reader1 = new BufferedReader(fileReader);
+                FileOutputStream fileReader = new FileOutputStream(fileName1);
+                ObjectOutputStream reader1 = new ObjectOutputStream(fileReader);
+                {
+                    reader1.writeObject(user);
+                }
             }
             catch (FileNotFoundException e) {
             System.err.println("Nie odnaleziono pliku " + fileName1);
@@ -20,6 +18,9 @@ public class FileManager {
             System.err.println("Błąd podczas zapisu danych do pliku " + fileName1);
         }
         }
+
+
+
     }
 
 
