2de023d56c616c3e74f67678ee1084309fde6027
Marcin <zapieful@gmail.com>
2018-07-23 19:46:20 +0200

    Update6

--- src/main/java/FileManager.java
+++ src/main/java/FileManager.java
@@ -8,8 +8,8 @@ public class FileManager {
 
         String fileName1 = "UserList.txt";
         try {
-            FileOutputStream fileReader = new FileOutputStream(fileName1);
-            ObjectOutputStream reader1 = new ObjectOutputStream(fileReader);
+            FileOutputStream fileReader1 = new FileOutputStream(fileName1);
+            ObjectOutputStream reader1 = new ObjectOutputStream(fileReader1);
             {
                 reader1.writeObject(user);
             }
@@ -25,16 +25,33 @@ public class FileManager {
 
         String fileName2 = "AuctionList.txt";
         try {
-            FileOutputStream fileReader = new FileOutputStream(fileName2);
-            ObjectOutputStream reader1 = new ObjectOutputStream(fileReader);
+            FileOutputStream fileReader2 = new FileOutputStream(fileName2);
+            ObjectOutputStream reader2 = new ObjectOutputStream(fileReader2);
             {
-                reader1.writeObject(auction);
+                reader2.writeObject(auction);
             }
         }
         catch (FileNotFoundException e) {
             System.err.println("Nie odnaleziono pliku " + fileName2);
         } catch (IOException e) {
             System.err.println("Błąd podczas zapisu danych do pliku " + fileName2);
+        }
+    }
+
+    public void saveOffersForAuction (Auction auction) {
+
+        String fileName3 = "OffersList.txt";
+        try {
+            FileOutputStream fileReader3 = new FileOutputStream(fileName3);
+            ObjectOutputStream reader3 = new ObjectOutputStream(fileReader3);
+            {
+                reader3.writeObject(auction);
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.err.println("Nie odnaleziono pliku " + fileName3);
+        } catch (IOException e) {
+            System.err.println("Błąd podczas zapisu danych do pliku " + fileName3);
         }
     }
 
