9121031214e6ee312ff86f52f05d96efb09e95d6
Marcin <zapieful@gmail.com>
2018-07-23 19:40:57 +0200

    Update5

--- src/main/java/FileManager.java
+++ src/main/java/FileManager.java
@@ -2,22 +2,41 @@ import java.io.*;
 
 public class FileManager {
 
+
+
     public void saveUserToList (User user) {
 
         String fileName1 = "UserList.txt";
-            try {
-                FileOutputStream fileReader = new FileOutputStream(fileName1);
-                ObjectOutputStream reader1 = new ObjectOutputStream(fileReader);
-                {
-                    reader1.writeObject(user);
-                }
+        try {
+            FileOutputStream fileReader = new FileOutputStream(fileName1);
+            ObjectOutputStream reader1 = new ObjectOutputStream(fileReader);
+            {
+                reader1.writeObject(user);
             }
-            catch (FileNotFoundException e) {
+        }
+        catch (FileNotFoundException e) {
             System.err.println("Nie odnaleziono pliku " + fileName1);
-            } catch (IOException e) {
+        } catch (IOException e) {
             System.err.println("Błąd podczas zapisu danych do pliku " + fileName1);
         }
+    }
+
+    public void saveAuctions (Auction auction) {
+
+        String fileName2 = "AuctionList.txt";
+        try {
+            FileOutputStream fileReader = new FileOutputStream(fileName2);
+            ObjectOutputStream reader1 = new ObjectOutputStream(fileReader);
+            {
+                reader1.writeObject(auction);
+            }
         }
+        catch (FileNotFoundException e) {
+            System.err.println("Nie odnaleziono pliku " + fileName2);
+        } catch (IOException e) {
+            System.err.println("Błąd podczas zapisu danych do pliku " + fileName2);
+        }
+    }
 
 
 
