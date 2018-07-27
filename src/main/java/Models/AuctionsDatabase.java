8d5ee0264d96598c555b4c5581295ba02209b7a8
Bartosz Kupajski <bartosz.kupajski@gmail.com>
2018-07-27 18:20:11 +0200

    Read and Load User from file

---
+++ src/main/java/Models/AuctionsDatabase.java
@@ -0,0 +1,20 @@
+package Models;
+
+import java.util.Map;
+
+public class AuctionsDatabase {
+    private static AuctionsDatabase instance;
+    private Map<String,Auction> currentAuctionsMap;
+    private Map<String,Auction> winningAuctionsMap;
+
+
+    public static AuctionsDatabase getInstance(){
+        if (instance == null){
+            instance = new AuctionsDatabase();
+        }
+        return instance;
+    }
+
+
+
+}
